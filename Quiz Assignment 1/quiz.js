bb060a6b7be3d0dc39f664fd3ee7e0a4737f385c
Joe Lynch <48054458+JLynchMedia@users.noreply.github.com>
2019-12-06 15:30:26 +0000

    Add files via upload

---
+++ Quiz Assignment 1/quiz.js
@@ -0,0 +1,3 @@
+// enter in your JS code here to generate the questions,keep track how many questions were entered correctly and to write output
+//to the screen indicating to the user how they got on 
+
